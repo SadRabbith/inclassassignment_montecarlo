87229485ceb52f765a27b3e8f8b55d615ae27c97
Chance Jackson <cjacks34@syr.edu>
2025-10-09 23:52:37 +0000

    prettified plot, added routine to calculate circumference (unfinished)

--- monte_carlo.py
+++ monte_carlo.py
@@ -25,22 +25,29 @@ print("Computed Value: ",integrate(ellipse,10000))
 print("Analytic Value: ",np.pi*5*2)
 
 
-N = np.arange(100,100000,1000) #
+N = np.arange(1,10000,100) #
 int_val = np.zeros(len(N))
 
 analytic_val = np.pi * 5 * 2
 for i in range(len(N)):
     int_val[i] += integrate(ellipse,N[i])
 
-plt.plot(N, np.abs(int_val-analytic_val))
+plt.plot(N, np.abs(int_val-analytic_val)/analytic_val)
+plt.plot(N, 1/np.sqrt(N),label=r"$1/\sqrt{N}$",linestyle='dashed',color='red')
 plt.xlabel(r"$N$", fontsize=14)
-plt.ylabel("Error", fontsize=14)
+plt.legend(fontsize=14)
+plt.ylabel("Relative Error", fontsize=14)
 plt.title("Error Scaling with Number of Samples",fontsize=14)
 plt.savefig("integral_error_scaling.png", dpi=300)
+plt.clf()
 
-################## FINDING CIRCUMFERENCE ##################
-ellipse_2 = lambda x: ellipse(x,5-0.00001, 2-0.00001)
-
+################## FINDING CIRCUMFERENCE #################
+epsilon = 0.1
+ellipse_2 = lambda x: ellipse(x, 5-epsilon, 2-epsilon)
+x = np.linspace(0,5,1000)
+plt.plot(x,ellipse(x))
+plt.plot(x,ellipse_2(x))
+plt.show()
 def circumference_estimate(f1,f2, N):
     hits = 0 #number of points inside area
     for i in range(N):
