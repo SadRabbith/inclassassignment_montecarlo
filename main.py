236f9ed06bb6e98e7749c3dea78c3171c2553261
sadman raabith <sraabi1@lsu.edu>
2025-10-09 11:31:47 -0400

    inverse

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import numpy as np 
+import matplotlib.pyplot as plt
+
+
+from scipy.stats import expon 
+
+# our function = exp(-x) / 1 - exp(-1)
+
+C_NORM = 1 / (1 - np.exp(-1))
+
+def target_pdf(x):
+    
+    # Use np.where to ensure the function exists only in the [0, 1] range
+    return np.where((x >= 0) & (x <= 1), C_NORM * np.exp(-x), 0)
+
+#  inverse CDF -> -ln(1 - u * (1 - e^-1))
+def inverse_cdf(u):
+   
+    return -np.log(1 - u * (1 - np.exp(-1)))
+
